01bcd344d3dec0f7f9c0b07fb7ac76bca3617311
Neilly <neilpilarca@gmail.com>
2023-02-13 17:36:49 +0100

    add watch route

--- project-watch/routes/watch.js
+++ project-watch/routes/watch.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
       method: "GET",
       url: "https://watch-database1.p.rapidapi.com/all-family-by/brandname/Rolex",
       headers: {
-        "X-RapidAPI-Key": "50448e6470mshcf846b608d3b6b5p1dd0cfjsn242262d2096e",
+        "X-RapidAPI-Key": "322cdd67b9msh825ad01241d6cccp1e397fjsnd3220b54691f",
         "X-RapidAPI-Host": "watch-database1.p.rapidapi.com",
       },
     });
@@ -36,13 +36,14 @@ router.get("/models", async (req, res) => {
       method: "GET",
       url: `https://watch-database1.p.rapidapi.com/all-models-by/brandname/Rolex/family/${family}`,
       headers: {
-        "X-RapidAPI-Key": "50448e6470mshcf846b608d3b6b5p1dd0cfjsn242262d2096e",
+        "X-RapidAPI-Key": "322cdd67b9msh825ad01241d6cccp1e397fjsnd3220b54691f",
         "X-RapidAPI-Host": "watch-database1.p.rapidapi.com",
       },
     });
     // res.send(response.data);
     // res.send("watch models will be here");
-    res.render("models", { response: response.data });
+    console.log(response.data);
+    res.render("models", { response: response.data, family: req.query.family });
   } catch (error) {
     console.error(error);
     res.send("An error occurred while retrieving the data");
@@ -50,24 +51,26 @@ router.get("/models", async (req, res) => {
 });
 
 // get specific model
-router.get("/models/:id", async (req, res) => {
-  const family = req.query.family;
-  const model = req.query.model;
-  console.log(model);
+router.get("/models/:family/:model", async (req, res) => {
+  const family = req.params.family;
+  const model = req.params.model;
+  console.log("family", family);
+  console.log("model", model);
   try {
     const response = await axios({
       method: "GET",
       url: `https://watch-database1.p.rapidapi.com/all-watches-by/brandname/Rolex/family/${family}/model/${model}`,
       // url: "https://watch-database1.p.rapidapi.com/all-watches-by/brandname/Omega/family/Aqua%20Terra/model/2005.75.00",
       headers: {
-        "X-RapidAPI-Key": "50448e6470mshcf846b608d3b6b5p1dd0cfjsn242262d2096e",
+        "X-RapidAPI-Key": "322cdd67b9msh825ad01241d6cccp1e397fjsnd3220b54691f",
         "X-RapidAPI-Host": "watch-database1.p.rapidapi.com",
       },
     });
     // console.log("hello from endpoibnt");
-    res.send(response.data);
+    // res.send(response.data);
     // res.send("fetching specific model");
-    // res.render("watch-details", { response: response.data });
+    console.log(response.data);
+    res.render("watch-details", { response: response.data });
   } catch (error) {
     console.error(error);
     res.send("An error occurred while retrieving the data");
